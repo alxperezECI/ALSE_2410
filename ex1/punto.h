161ebdd487922c2d8b25f5dd589258eaf9e151f7
Alexander Pérez <alexander.perez@escuelaing.edu.co>
2024-02-22 15:08:54 -0500

    Convirtiendo las funciones en miembros de la estructura.

--- ex1/punto.h
+++ ex1/punto.h
@@ -7,8 +7,23 @@ struct punto_st{
     punto_st(){
       x = y = 0.0;
     }
+    
+    punto_st operator+( const punto_st &a ){
+      punto_st res;
+      res.x = x + a.x;
+      res.y = y + a.y;
+      
+      return res;
+    }
+
+    punto_st& operator+=( punto_st &a ){
+        x += a.x;
+        y += a.y;
+        return *this;
+    }
 };    
 
+/*
 punto_st operator+(const punto_st &a, const punto_st &b){
     punto_st res;
     // No se pueden modificar los parámetros ya que son de solo lectura    a.x = 8.;
@@ -22,7 +37,7 @@ punto_st& operator+=( punto_st &a, const punto_st &b){
     a.y += b.y;
     return a;
 }
-
+*/
 ostream& operator<<(ostream &out, const punto_st &b){
     out << "( " << b.x << ", " << b.y << " )";
     return out;
