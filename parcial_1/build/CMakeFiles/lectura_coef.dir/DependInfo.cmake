d6dbc35b1a6a3243efc683b1a6933651576be723
feddericocar <federico.cardona-m@mail.escuelaing.edu.co>
2024-02-23 16:04:10 -0500

    Parcial Brigitte y Federico

---
+++ parcial_1/build/CMakeFiles/lectura_coef.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fedec/exmpl/ALSE_2410/parcial_1/parcial.cpp" "CMakeFiles/lectura_coef.dir/parcial.cpp.o" "gcc" "CMakeFiles/lectura_coef.dir/parcial.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- parcial_1/parcial.cpp
+++ parcial_1/parcial.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 struct complex{
     double re, im;
 };
+
+double mag(double im, double re){
+  double mag_ = im*im + re*re;
+  mag_ = sqrt(mag_);
+  return mag_;
+}
+
+double ag(double im, double re){
+  double ag_ = im/re;
+  ag_ = atan(ag_);
+  return ag_;
+}
 
 int main(int argc, char** argv){
   if(argc != 2){
@@ -37,7 +50,28 @@ int main(int argc, char** argv){
     // Mostrar en pantalla el número, su magnitud y su ángulo;
     // Encontrar el número con mayor magnitud y el número con mayor ángulo
     // y mostrarlos en la pantalla.
-
+  complex numeros_complejos[10];
+  complex max_mag, max_ag;
+  double mag_max = 0,ag_max = 0;
+  for (int i = 0; i<10 ; i++){
+    numeros_complejos[i].re = datos_int[2*i];
+    numeros_complejos[i].im = datos_int[2*i+1];
+    double angulo = ag(numeros_complejos[i].im, numeros_complejos[i].re);
+    double magnitud = mag(numeros_complejos[i].im, numeros_complejos[i].re);
+    cout<<"El númro complejo es: "<<numeros_complejos[i].re<<" + "<<numeros_complejos[i].im<<"i"<<endl; 
+    cout<<"La magntud es: "<<magnitud<<endl;
+    cout<<"El ángulo es: "<<angulo<<"rad"<<endl;
+    if(magnitud > mag_max){
+      mag_max = magnitud;
+      max_mag = numeros_complejos[i];
+    }
+    if(angulo > ag_max){
+      ag_max = angulo;
+      max_ag = numeros_complejos[i];
+    }
+  }
+  cout<<"El numero con mayor magnitud es: "<<max_mag.re<<" + "<<max_mag.im<<"i"<<endl; 
+  cout<<"El numero con mayor ángulo es: "<<max_ag.re<<" + "<<max_ag.im<<"i"<<endl; 
 
 
 
