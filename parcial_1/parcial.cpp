5f376c59e3849684a9be0a7470cd1e83ceb0ce02
DiegoAT26 <159958204+DiegoAT26@users.noreply.github.com>
2024-02-25 18:44:59 -0500

    Update parcial.cpp
    version parcial 1

--- parcial_1/parcial.cpp
+++ parcial_1/parcial.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+# include <cmath>
 
 using namespace std;
 struct complex{
     double re, im;
 };
+
+
+// Crear una función que calcule la magnitud del número complejo
+
+double magnitud(const complex& c) {
+    return sqrt(pow(c.re, 2) + pow(c.im, 2));
+}
+
+
+// Crear una funcion que calcule el ángulo del número complejo
+double angulo(const complex& c) {
+    return atan2(c.im, c.re);
+}
+
 
 int main(int argc, char** argv){
   if(argc != 2){
@@ -23,7 +38,7 @@ int main(int argc, char** argv){
   cout << "Se abrirá el archivo: " << path << endl;
 
   // Variables definidas para el proceso
-  int  datos_int[20]={0}; // FIXME: Agregar un vector para almacenar los números convertidos.
+  int  datos_int[20]={0}; //  Agregar un vector para almacenar los números convertidos.
 
   if(archivo.is_open()){
     for(int i = 0; i < 20; i++){
@@ -31,12 +46,56 @@ int main(int argc, char** argv){
       datos_int[i] = stoi( path );
       cout << datos_int[i] << endl;
     }
+
     // Con los datos que se van a cargar, crear un vector de 10 complejos que usen la estructura
-    // Crear una función que calcule la magnitud del número complejo
-    // Crear una funcion que calcule el ángulo del número complejo
-    // Mostrar en pantalla el número, su magnitud y su ángulo;
+   
+    complex numeros_comp[10];
+    for (int i = 0; i < 10; i++) {
+            complex c;
+            numeros_comp[i].re = datos_int[2*i];
+            numeros_comp[i].im = datos_int[2*i + 1];
+            
+
+            // Mostrar en pantalla el número, su magnitud y su ángulo;
+            cout << "Número: " << c.re << " + " << c.im << "i, ";
+            cout << "Magnitud: " << magnitud(c) << ", ";
+            cout << "Ángulo: " << angulo(c) << " rad" << endl;
+        }
+
+
+    
     // Encontrar el número con mayor magnitud y el número con mayor ángulo
     // y mostrarlos en la pantalla.
+
+     
+        double maxMagnitude = 0;
+        complex maxMagnitudeNumber;
+
+        for (const complex& c : numeros_comp) {
+            double magnitude = magnitud(c);
+            if (magnitude > maxMagnitude) {
+                maxMagnitude = magnitude;
+                maxMagnitudeNumber = c;
+            }
+        }
+
+        
+        double maxAngle = 0;
+        complex maxAngleNumber;
+
+        for (const complex& c : numeros_comp) {
+            double angle = angulo(c);
+            if (angle > maxAngle) {
+                maxAngle = angle;
+                maxAngleNumber = c;
+            }
+        }
+
+      
+        cout << "Número con mayor magnitud: " << maxMagnitudeNumber.re << " + " << maxMagnitudeNumber.im << "i" << endl;
+        cout << "Número con mayor ángulo: " << maxAngleNumber.re << " + " << maxAngleNumber.im << "i" << endl;
+
+
 
 
 
@@ -50,7 +109,6 @@ int main(int argc, char** argv){
   }
   return 0;
 }
-
 
 
 
